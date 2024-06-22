ea0f7295003cd7a4715e24a07ab508bed048bc34
alec sharp <alecsharpie@gmail.com>
2024-06-23 01:54:06 +1000

    optimise build

--- pi.sh
+++ pi.sh
@@ -1,7 +1,3 @@
 PI="pi@192.168.0.12"
 
-zip -r build.zip fun
-ssh $PI "rm -rf ~/pi/fun"
-ssh $PI "mkdir ~/pi/fun"
-scp build.zip $PI:~/pi/fun/build.zip
-ssh $PI "unzip -o ~/pi/fun/build.zip -d ~/pi/fun"
+rsync -avz --delete fun/ $PI:~/fun/
